0caa4f6b93aa20b8eda98149635c152fefb936b8
Fido <mymainhelper@gmail.com>
2022-05-12 00:39:36 +0200

    Add better files

---
+++ UncompiledPlugin/src/de/fido/VarManager/VManager.java
@@ -0,0 +1,10 @@
+package de.fido.VarManager;
+
+import org.bukkit.entity.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VManager {
+    public static List<Entity> listOfAllEntitys = new ArrayList<Entity>();
+}
